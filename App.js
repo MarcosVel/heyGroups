b7cb92d384008e41bd7a2c44413c407a870235a0
MarcosVel <marcos.veloso@sempreceub.com>
2022-10-01 16:52:37 -0300

    feat: header styles

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
-import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
 import Routes from "./src/routes";
-import firebase from "./src/services/firebaseConnection";
 
 export default function App() {
   return (
--- src/pages/ChatRoom/index.jsx
+++ src/pages/ChatRoom/index.jsx
@@ -1,22 +1,60 @@
+import { Ionicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
-import { Button, StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function ChatRoom() {
   const navigation = useNavigation();
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="light-content" backgroundColor="#2E54D4" />
+      <View style={styles.header}>
+        <View style={styles.chatBack}>
+          <TouchableOpacity>
+            <Ionicons name="arrow-back-outline" size={28} color="#fff" />
+          </TouchableOpacity>
+          <Text style={styles.title}>Grupos</Text>
+        </View>
+        <TouchableOpacity>
+          <Ionicons name="search" size={28} color="#fff" />
+        </TouchableOpacity>
+      </View>
       <Text>ChatRoom</Text>
       <Button title="Login" onPress={() => navigation.navigate("Auth")} />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+  },
+  header: {
+    flexDirection: "row",
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "space-between",
+    paddingBottom: 16,
+    paddingHorizontal: 16,
+    backgroundColor: "#2E54D4",
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+  },
+  chatBack: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  title: {
+    marginLeft: 8,
+    fontSize: 28,
+    fontWeight: "bold",
+    color: "#FFF",
   },
 });
