b16f51f79954d122f7cd74c2014e6a22d2309544
MarcosVel <marcos.veloso@sempreceub.com>
2022-10-11 00:42:55 -0300

    feat: mapping messages realtime

--- src/pages/Messages/index.jsx
+++ src/pages/Messages/index.jsx
@@ -1,7 +1,45 @@
+import { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import firebase from "../../services/firebaseConnection";
 
 export default function Messages({ route }) {
   const { thread } = route.params;
+  const [messages, setMessages] = useState([]);
+  const user = firebase.auth().currentUser.toJSON();
+
+  useEffect(() => {
+    const unsubscribeListener = firebase
+      .firestore()
+      .collection("MESSAGE_THREADS")
+      .doc(thread._id)
+      .collection("messages")
+      .orderBy("createdAt", "desc")
+      .onSnapshot(querySnapshot => {
+        const groupMessages = querySnapshot.docs.map(doc => {
+          const firebaseData = doc.data();
+
+          const data = {
+            _id: doc.id,
+            text: "",
+            createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+            ...firebaseData,
+          };
+
+          if (!firebaseData.system) {
+            data.user = {
+              ...firebaseData.user,
+              name: firebaseData.user.displayName,
+            };
+          }
+
+          return data;
+        });
+
+        setMessages(groupMessages);
+      });
+
+    return () => unsubscribeListener();
+  }, []);
 
   return (
     <View style={styles.container}>
